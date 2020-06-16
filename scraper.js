6a096b9cb9a9cec6c8e764be5a7efd76ea4048c7
Helena Gagern <helena.gagern@gmail.com>
2020-06-16 21:51:14 +0200

    attempt to debug

--- scraper.js
+++ scraper.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer')
 function run () {
   return new Promise(async (resolve, reject) => {
     try {
-      const browser = await puppeteer.launch({headless: false});
+      const browser = await puppeteer.launch({headless: false, slowMo: 200 });
       const page = await browser.newPage();
       await page.goto('https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=W&offices=AL,AT,BA,BG,BX,CH,CY,CZ,DE,DK,EE,ES,FI,FR,GB,GR,HR,HU,IE,IS,IT,LI,LT,LV,MC,MD,ME,MK,MT,NO,PL,PT,RO,RS,RU,SE,SI,SK,SM,US,EM,WO&territories=AT,BE,BG,HR,CY,CZ,DK,EE,FI,FR,DE,GR,HU,IE,IT,LV,LT,LU,MT,NL,PL,PT,RO,SK,SI,ES,SE,GB,AX,AL,AD,BY,BQ,BA,CW,FO,GI,GG,IS,IM,JE,LI,MD,MC,ME,MK,NO,RU,SH,SM,RS,SX,SJ,CH,UA,VA,US&basicSearch=ace&niceClass=9,OR,35,OR,41,OR,42&tmStatus=Filed,Registered')
       await page.waitForSelector('button.sc-hycgNl.inAWdR.sc-ekkqgF.dkdxYu.sc-jTzLTM.iNBqCm', {timeout: 0});
@@ -14,7 +14,7 @@ function run () {
       console.log("Clicked button")
       let currentPage = 1;
       let urls = [];
-      while (currentPage <= 3) {
+      while (currentPage <= 5) {
         let newUrls = await page.evaluate(() => {
           let results = [];
           console.log("Selecting description")
@@ -31,13 +31,13 @@ function run () {
           return results
         });
         urls = urls.concat(newUrls);
-        if (currentPage < 3) {
+        if (currentPage < 5) {
           await Promise.all([
             await page.click('a.sc-lhVmIH.TfOQd.sc-kvZOFW.bHpWxE'),
-            await page.waitForSelector('.sc-lnmtFM')
 
             ])
-          console.log("Looking for next page")
+          page.once('load', () => console.info('✅ Page is loaded'));
+          page.on('error', error => console.error(`❌ ${error}`));
         }
         currentPage++;
       }

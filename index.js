028a75e24ec8cfbfe3ea2a0dc6d06178f09c222c
Helena Gagern <helena.gagern@gmail.com>
2020-06-20 20:34:17 +0200

    changing url to take a screenshot of business model canvas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ void (async () => {
   try {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto('https://www.tmdn.org/tmview/#/tmview/results?page=1&pageSize=30&criteria=W&offices=AL,AT,BA,BG,BX,CH,CY,CZ,DE,DK,EE,ES,FI,FR,GB,GR,HR,HU,IE,IS,IT,LI,LT,LV,MC,MD,ME,MK,MT,NO,PL,PT,RO,RS,RU,SE,SI,SK,SM,US,EM,WO&territories=AT,BE,BG,HR,CY,CZ,DK,EE,FI,FR,DE,GR,HU,IE,IT,LV,LT,LU,MT,NL,PL,PT,RO,SK,SI,ES,SE,GB,AX,AL,AD,BY,BQ,BA,CW,FO,GI,GG,IS,IM,JE,LI,MD,MC,ME,MK,NO,RU,SH,SM,RS,SX,SJ,CH,UA,VA,US&basicSearch=ace&niceClass=9,OR,35,OR,41,OR,42')
+    await page.goto('https://drive.google.com/file/d/0B3HebLpkReCeYV9DRWtEQVF1SWM/view')
     await page.waitFor(50000);
     await page.screenshot({
       path: './screenshots/page1.png'
